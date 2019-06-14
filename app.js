7c853f0f89d938b4654754dc09262548f087a060
vmasdani <valianmasdani@gmail.com>
2019-06-14 22:41:00 +0700

    Added final

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Express and sqlite declaration
 const express = require('express');
+const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 
 // Database initialization
@@ -9,30 +10,117 @@ let sched = new sqlite3.Database('./db/schedule.db', (err) => {
   }
   console.log('Connected to schedule database.');
 });
-
-let waterlength = new sqlite3.Database('./db/waterlength.db', (err) => {
-  if(err) {
-    console.error(err.message);
-  }
-  console.log('Connedted to waterlength database');
-});
 // Database initialization end
 
 // Express app initialization
 const app = express();
-const port = 3000;
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+const port = 80;
 
-/*
-waterlength.all(`select * from length`, [], (err, rows) => {
-  console.log('waterlength data: ', rows);
+// mqtt connection
+const host = 'mqtt://broker.hivemq.com';
+const mqtt = require('mqtt');
+const client = mqtt.connect(host);
+
+client.on('connect', () => {
+  console.log('Connected to MQTT broker!');
 });
 
-sched.all(`select * from sched`, [], (err, rows) => {
-  console.log('schedule data: ', rows);
-});
-*/
 app.get('/', (req, res) => {
-  res.send('Hello warudo');
+  res.sendFile('/index.html');
+});
+
+app.post('/sched', (req, res) => {
+  console.log('Adding schedule...');
+  const data = req.body;
+  const hour = data.hour;
+  const min = data.min;
+
+  const query = `insert into sched values(null, ${hour}, ${min})`;
+
+  sched.get(query, [], (err) => {
+    if(err) {
+      throw err;
+    }
+  });
+  console.log(data);
+  res.sendStatus(201);
+});
+
+app.get('/sched', (req, res) => {
+  console.log('Adding schedule...');
+  res.setHeader('Content-Type', 'application/json');
+
+  const query = 'select * from sched';
+  sched.all(query, [], (err, rows) => {
+    if(err) {
+      res.send(err);
+    }
+    res.send(JSON.stringify(rows));
+  });
+});
+
+app.delete('/sched', (req, res) => {
+  console.log('Deleting schedule...');
+  const data = req.body;
+  const query = `delete from sched where id=${data.id}`;
+
+  console.log(data, query);
+
+  sched.get(query, [], (err) => {
+    if(err) {
+      res.send(err);
+      throw err;
+    }
+    else {
+      res.sendStatus(204);
+    }
+  });
+});
+
+app.get('/length', (req, res) => {
+  console.log('Getting length...');
+  res.setHeader('Content-Type', 'application/json');
+
+  const query = 'select * from length where id=1';
+  sched.get(query, [], (err, rows) => {
+    if(err) {
+      res.send(err);
+    }
+    res.send(JSON.stringify(rows));
+  });
+});
+
+app.put('/length', (req, res) => {
+  console.log('Updating length...');
+  const data = req.body;
+  const mins = data.mins;
+  const secs = data.secs;
+  console.log({
+    mins: mins,
+    secs: secs,
+  });
+
+  const query = `update length set minute=${mins}, second=${secs} where id=1`;
+  sched.get(query, [], (err) => {
+    if(err) {
+      throw err;
+      res.send(err);
+    }
+    else {
+      res.sendStatus(202);
+    }
+  })
+});
+
+// mqtt route
+app.post('/control', (req, res) => {
+  const data = req.body; 
+  controlData = data.control;
+  console.log('Control data:', controlData.toString());
+
+  client.publish('control', controlData.toString());
 });
 
 // Start express app  
